2f58ed9ddf6c2cdd8f1688e5c631bccf933fe009
Serhii Yakubovych <yasyncsm@gmail.com>
2016-11-09 00:02:23 +0200

    Cut a head block from main page to separete file head.php and print link to him

--- views/main_page.php
+++ views/main_page.php
@@ -1,23 +1,9 @@
 <!DOCTYPE html>
 <html>
     
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="keywords" content="Юридична компанія">
-        <meta name="description" content="Міжнародна юридична компанія 'Александров і партнер'и створена в 2002 році адвокатами, що мають великий досвід роботи в різних галузях права, здатними розв'язати найскладнішу проблему.">
-        <link rel="icon" href="favicon.ico">
-        <link rel="stylesheet" href="css/style.css">
-        <title>Міжнародна юридична компанія "Александров і партнери"</title>
-        <script src="js/jquery-3.1.1.min.js"></script>
-        
-        <!--Для старих версій IE 9-->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-    </head>
+    <?php
+        include("head.php");
+    ?>
     
     <body>
         
